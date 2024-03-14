ca82a51ca569ca00720a889b5ac5e5d2b58f6b4d
JoshKoiro <josh.koiro@gmail.com>
2024-03-14 09:32:53 -0400

    for real completed #15 - fixing timing issues

--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -24,37 +24,50 @@ func NewTimer(seconds ...int) *Timer {
 }
 
 func (t *Timer) Start(key string, duration int) {
-	endTime := time.Now().Add(t.Duration)
+	endTime := time.Now().Add(time.Duration(duration) * time.Second)
 
-	for range time.Tick(time.Second) {
-		if time.Now().After(endTime) {
-			//clear the last line from console output
-			fmt.Printf("\033[2K\r")
-			c := color.New(color.FgHiGreen)
-			c.Println("\n🍅Pomodoro completed!🍅\n")
-			break
-		} else {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
 			remainingTime := endTime.Sub(time.Now()).Truncate(time.Second)
 			minutes := remainingTime / time.Minute
 			seconds := remainingTime % time.Minute / time.Second
-
-			c := color.New(color.FgHiCyan)
-			c.Printf("\033[2K\rTime remaining: %d m %d sec", minutes, seconds)
-		}
-	}
-
-	// update the teams status message every minute
-	for range time.Tick(time.Minute) {
-		// if duration/60 is 1 or less minutes, update the status message to say minute left instead of minutes left
-		if duration/60 <= 1 {
-			teamsError := teamsapi.SetStatusMessage(key, fmt.Sprintf("Pomodoro: %d minute left", duration/60), "")
-			if teamsError != nil {
-				fmt.Println(teamsError)
+			if time.Now().After(endTime) {
+				// Clear the last line from console output
+				fmt.Printf("\033[2K\r")
+				c := color.New(color.FgHiGreen)
+				c.Println("\n🍅Pomodoro completed!🍅\n")
+				return
+			} else {
+				// Update the console output
+				c := color.New(color.FgHiCyan)
+				c.Printf("\033[2K\rTime remaining: %d m %d sec", minutes, seconds)
 			}
-		}
-		teamsError := teamsapi.SetStatusMessage(key, fmt.Sprintf("Pomodoro: %d minutes left", duration/60), "")
-		if teamsError != nil {
-			fmt.Println(teamsError)
+
+			// Update the teams status message every minute
+			if time.Now().Second()%60 == 0 {
+				// If duration/60 is 1 or less minutes, update the status message to say minute left instead of minutes left
+				if minutes == 1 {
+					teamsError := teamsapi.SetStatusMessage(key, "Busy in a Pomodoro session for the next "+fmt.Sprintf("%d minute...🍅", minutes), "")
+					if teamsError != nil {
+						fmt.Println(teamsError)
+					}
+				} else if minutes < 1 {
+					teamsError := teamsapi.SetStatusMessage(key, "Less than a minute left in a Pomodoro session...🍅", "")
+					if teamsError != nil {
+						fmt.Println(teamsError)
+					}
+				} else {
+					teamsError := teamsapi.SetStatusMessage(key, "Busy in a Pomodoro session for the next "+fmt.Sprintf("%d minutes...🍅", minutes), "")
+					if teamsError != nil {
+						fmt.Println(teamsError)
+					}
+				}
+
+			}
 		}
 	}
 }
